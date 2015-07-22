f8418ac4f69a3df84a2213924eababd93ed8e711
Hailey Foster <haileyhasmail@gmail.com>
2015-07-22 16:55:01 -0700

    Pseudocode for POST

--- web/request-handler.js
+++ web/request-handler.js
@@ -3,6 +3,8 @@ var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
 var httpHelper = require('./http-helpers.js');
 
+//make sendResponse helper; includes setting statusCode and headers
+
 exports.handleRequest = function (req, res) {
 
   if (req.method === 'GET'){
@@ -17,7 +19,14 @@ exports.handleRequest = function (req, res) {
     res.writeHead(statusCode, httpHelper.headers);
     httpHelper.serveAssets(res, url, res.end);
   }
-
+  //check for POST req.method
+    //set event listener for on data
+      //build up data string with chunks 
+    //update statusCode to 302
+    //write header as before
+    //set event listener for on end
+      //write data to file
+      //end response  
   res.end(archive.paths.list);
 };
 
