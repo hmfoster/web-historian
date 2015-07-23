250e45240be1e8d4d869804a36542a71b9df7e59
Hailey Foster <haileyhasmail@gmail.com>
2015-07-22 17:30:21 -0700

    Append new urls to sites.txt

--- web/request-handler.js
+++ web/request-handler.js
@@ -2,6 +2,7 @@ var path = require('path');
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
 var httpHelper = require('./http-helpers.js');
+var fs = require('fs');
 
 //make sendResponse helper; includes setting statusCode and headers
 
@@ -20,13 +21,35 @@ exports.handleRequest = function (req, res) {
     httpHelper.serveAssets(res, url, res.end);
   }
   //check for POST req.method
+  else if (req.method === 'POST'){
     //set event listener for on data
-      //build up data string with chunks 
+    var dataString = '';
+    req.on('data',function(chunk){
+      //build up data string with chunks
+      dataString +=chunk.toString();
+    });
     //update statusCode to 302
+    //statusCode = 302;
     //write header as before
+    res.writeHead(302, httpHelper.headers);
     //set event listener for on end
-      //write data to file
-      //end response  
+    req.on('end',function(){
+      dataString = dataString.split('=')[1]+"\n";
+      //write data to file --> use appendFile, passing in data
+      fs.appendFile(archive.paths.list, dataString, function(err){
+        if (err){
+          throw err;
+        }
+      });
+      res.end();
+    });
+      //end response --> as a callback to append???
+    }
   res.end(archive.paths.list);
 };
 
+// add to archive-helpers
+// fs.appendFile('message.txt', 'data to append', function (err) {
+//   if (err) throw err;
+//   console.log('The "data to append" was appended to file!');
+// });
